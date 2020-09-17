f1af79a16db1c37ff418b436e4852d7ea6a544d3
0xBECEDA <sakharovasonya1@gmail.com>
2020-09-17 03:00:59 +0300

    dialog

--- dialog_client.sh
+++ dialog_client.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 
-echo "Begin\n"
-screen -S "try" -d -m -L
+SERVER=server
+CLIENT=client
+DELAY=3
+if test -x "$SERVER" && test -x "$CLIENT"; then
+    screen -S "server" -d -m -L
+    screen -S server -p 0 -X stuff './server\n'
+    sleep $DELAY
 
-sleep 1
+    screen -S "client_1" -d -m
+    sleep $DELAY
 
-# screen -S try -X stuff 'script log.txt'$(echo -ne '\015')
+    screen -S client_1 -p 0 -X stuff './client  > client_1.txt \n'
+    sleep $DELAY
 
-screen -S try -X stuff 'script log.txt\n'
+    screen -S "client_2" -d -m
+    sleep $DELAY
 
-sleep 1
+    screen -S client_2 -p 0 -X stuff './client  > client_2.txt\n'
+    sleep 3
 
-# screen -S try -X stuff 'echo Hello!'$(echo -ne '\015')
-screen -S try -X stuff 'echo Hello!\n'
-sleep 1
+    # общение
+    screen -S client_2 -p 0 -X stuff '"Hi!"\n'
+    sleep $DELAY
+    screen -S client_1 -p 0 -X stuff '"Hello!"\n'
+    sleep $DELAY
+    screen -S client_1 -p 0 -X stuff '"How are you?"\n'
+    sleep $DELAY
+    screen -S client_2 -p 0 -X stuff '" I am fine"\n'
+    sleep $DELAY
+    screen -S client_1 -p 0 -X stuff '"Buy :)"\n'
+    sleep $DELAY
 
-# screen -S try -X stuff 'exit'$(echo -ne '\015')
-screen -S try -X stuff 'exit\n'
-sleep 1
+    screen -S client_1 -p 0 -X stuff 'exit\n'
+    screen -S client_2 -p 0 -X stuff 'exit\n'
+    screen -S server -p 0 -X stuff 'exit\n'
 
-screen -X -S try kill
+    screen -X -S server kill
+    screen -X -S client_1 kill
+    screen -X -S client_2 kill
 
-echo "end\n"
+else
+    echo "$SERVER или $CLIENT не существует\n"
+fi
